8729e661702ad690af843d9a2fc89b5151e6657c
Guillaume Chau <guillaume.b.chau@gmail.com>
2016-06-17 22:32:55 +0200

    Nested routes

--- client/routes.js
+++ client/routes.js
@@ -3,6 +3,8 @@ import {Router} from 'meteor/akryum:vue-router';
 // Components
 import Home from '/imports/ui/Home.vue';
 import Page1 from '/imports/ui/Page1.vue';
+import Page1A from '/imports/ui/Page1A.vue';
+import Page1B from '/imports/ui/Page1B.vue';
 import Page2 from '/imports/ui/Page2.vue';
 
 Router.map({
@@ -12,7 +14,18 @@ Router.map({
   },
   '/page1': {
     name: 'page1',
-    component: Page1
+    component: Page1,
+    // Nested routes
+    subRoutes: {
+      '/a': {
+        name: 'page1a',
+        component: Page1A
+      },
+      '/b': {
+        name: 'page1b',
+        component: Page1B
+      }
+    }
   },
   '/page2': {
     name: 'page2',
--- imports/ui/Page1.vue
+++ imports/ui/Page1.vue
@@ -2,5 +2,28 @@
   <div class="page1">
     <h1>Page 1</h1>
     <p>You are in the first page</p>
+    <nav>
+      <a v-link="{ name:'page1a' }">Page A</a>
+      <a v-link="{ name:'page1b' }">Page B</a>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+nav {
+  text-align: center;
+  margin: 12px 0;
+  padding: 12px 0;
+  border-top: solid 1px #40b883;
+  border-bottom: solid 1px #40b883;
+}
+nav a {
+  padding: 6px;
+  border-radius: 3px;
+}
+nav a.v-link-active {
+  background: #40b883;
+  color: white;
+}
+</style>
